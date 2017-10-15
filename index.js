c463256aacbd782ff9f6b0f4cb4af0e3f9b476bf
Sherry Mei <sherrymei95@gmail.com>
2017-10-14 22:06:34 -0400

    changed variable server to variable app

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 })
 
  app.listen(process.env.PORT || 3000, function() {
-    console.log('Listening on port %s!', server.address().port)
+    console.log('Listening on port %s!', app.address().port)
 })
 
 // var a = `
